435913dbf34547585f676fecdef3c982126de762
Tushar Bendre <tusharbendre01@gmail.com>
2024-06-09 10:23:11 +0530

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Tushar Bendre on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
